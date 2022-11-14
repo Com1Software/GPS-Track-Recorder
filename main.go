c6d1e6e4fbfd947fa1ff531391cfc4827f7aca2b
Com1 Software, Inc <information@com1software.com>
2022-11-14 10:27:42 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
+package main
 
+import (
+	"fmt"
+	"os/exec"
+	)
+
+//-------------------------------------------------------------------------
+func main() {
+	fmt.Println("GPS-Track-Recorder")
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+}
